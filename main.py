76f838b41d10103a0581284368c6ca2346df4246
Zhenya <mindoolla1289@gmail.com>
2022-05-09 18:16:09 +0600

    Создаем главное приложение и настраеваем uvicorn

---
+++ main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+import uvicorn 
+
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", port=8000, host="127.0.0.1", reload=True)
