2ba19b8706c2c81f4d0eaa96dbb7fc56c41c256e
Zhenya <mindoolla1289@gmail.com>
2022-05-10 20:00:04 +0600

    Создал докер файл .yaml,с настройками контейнера, для запуска контейнера, так же был создан файл .env для храннения переменных окружения , прерименовал переменную database на dbase.

--- db/base.py
+++ db/base.py
@@ -1,10 +1,18 @@
-from importlib.metadata import metadata
+
+
+
 from databases import Database
 from sqlalchemy import create_engine, MetaData
+
+
+
 from core.config import DATABASE_URL
 
-database = Database(DATABASE_URL)
+
+
+
+dbase = Database(DATABASE_URL)
 metadata = MetaData()
 engine = create_engine(
     DATABASE_URL,
-) 
+)
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from db.base import database
+from db.base import dbase
 import uvicorn 
 
 
@@ -9,13 +9,15 @@ app = FastAPI()
 async def root():
     return {"message": "Hello World"}
 
+
+
 @app.on_event("startup")
 async def startup():
-    await database.connect()
+    await dbase.connect()
 
 @app.on_event("shutdown")
 async def shutdown():
-    await database.disconnect()
+    await dbase.disconnect()
 
 
 if __name__ == "__main__":
