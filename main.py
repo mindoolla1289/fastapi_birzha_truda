6bbd02d88611bd4271509f38a1afb09a91a5a260
Zhenya <mindoolla1289@gmail.com>
2022-05-10 11:21:49 +0600

    Подключились к базе данных, создали два евента при старте программы и при отключении

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from db.base import database
 import uvicorn 
 
 
@@ -7,6 +8,15 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.on_event("startup")
+async def startup():
+    await database.connect()
+
+@app.on_event("shutdown")
+async def shutdown():
+    await database.disconnect()
+
 
 if __name__ == "__main__":
     uvicorn.run("main:app", port=8000, host="127.0.0.1", reload=True)
