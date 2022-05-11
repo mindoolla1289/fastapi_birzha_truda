5ad69687716f38e1c2d59fa1901d0e492dfd7789
Zhenya <mindoolla1289@gmail.com>
2022-05-11 20:27:28 +0600

    Исправил репозиторий users.py и связал его с моделью user

--- repositories/users.py
+++ repositories/users.py
@@ -1,20 +1,22 @@
 from .base import BaseRepository
 from db.users import users
+from typing import List
+from models.user import User, UserIn
 
 
 class UserRepository(BaseRepository):
 
-    async def get_all(limit: int=100, skip: int=0):
+    async def get_all(self, limit: int=100, skip: int=0) -> List[User]:
         return
     
-    async def get_by_id(id:int):
+    async def get_by_id(self, id:int) -> User:
         return
     
-    async def creat():
+    async def creat(self, u:UserIn) -> User:
         return
     
-    async def update():
+    async def update(self, u:UserIn) -> User:
         return
 
-    async def get_by_email(email: str):
+    async def get_by_email(self, email: str) -> User:
         return
