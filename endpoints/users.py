2af2311791e89abd8d81837b28c7d94451e6fc2b
Zhenya <mindoolla1289@gmail.com>
2022-06-07 05:41:21 +0600

    в данном случае используем для создание, редактирование и получения списка пользователей.Также для более понятной архитектуры будем использовать router-ы, импортировал созданный роутер в main.py

--- endpoints/users.py
+++ endpoints/users.py
@@ -1,7 +1,12 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from typing import List
+from repositories.users import UserRepository
+from models.user import User, UserIn
+from .depends import get_users_repository
 
 router = APIRouter()
 
 @router.get("/")
 async def read_users(limit: int =100, skip: int = 100):
+
     return{}
