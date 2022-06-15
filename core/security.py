0aec93b19c2468506e6a368edd43e60a1333ea43
Zhenya <mindoolla1289@gmail.com>
2022-06-15 18:32:00 +0600

    начинаем реализацию аутентификации,будем использовать JWT токены,python- jose,создал функции для создания и декодирования токенов

--- core/security.py
+++ core/security.py
@@ -1,4 +1,7 @@
 from  passlib.context import CryptContext
+import datetime
+from jose import jwt 
+from .config import ACCESS_TOKEN_EXPIRE_MINUTES, SECRET_KEY, ALGORITHM
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated = "auto")
 
@@ -7,3 +10,16 @@ def hash_password(password: str) -> str:
 
 def verify_password(password:str, hash: str) -> str:
     return pwd_context.verify_password(password, hash)
+
+
+def create_access_token(data:dict) -> str:
+    to_encode= data.copy()
+    to_encode.update({"exp":datetime.datetime.utcnow()+ datetime.timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)})
+    return jwt.encode(to_encode,SECRET_KEY,algorithm=ALGORITHM)
+
+def decode_access_token(token:str):
+    try:
+        encode_jwt= jwt.decode(token,SECRET_KEY,algorithms=ALGORITHM)
+    except jwt.JWSError:
+        return None
+    return encode_jwt
