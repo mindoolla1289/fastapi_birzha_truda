7e1d26ea2c5adefca96619b7269095c3454d5fff
Zhenya <mindoolla1289@gmail.com>
2022-06-17 03:57:19 +0600

    Теперь заставим юзера отправить этот токен и будем проверять токен на подлинность, защита будет состоять из двух этапов. Первая сущность которая будет доставать и проверять токен

--- core/security.py
+++ core/security.py
@@ -1,3 +1,5 @@
+from fastapi import Request, HTTPException, status
+from fastapi.security import HTTPBearer
 from  passlib.context import CryptContext
 import datetime
 from jose import jwt 
@@ -23,3 +25,18 @@ def decode_access_token(token:str):
     except jwt.JWSError:
         return None
     return encode_jwt
+
+class JWTBearer(HTTPBearer):
+    def __init__(self, auto_error: bool= True):
+        super(JWTBearer, self).__init__(auto_error=auto_error)
+    
+    async def __call__(self, request: Request):
+        credentials: HTTPAuthorizationCredentials = await super(JWTBearer, self).__call__(request)
+        exp = HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Invalid auth token")
+        if credentials:
+            token = decode_access_token(credentials.credentials)
+            if token is None:
+                raise exp
+            return credentials.credentials
+        else:
+            raise exp
